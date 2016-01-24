6fc9d9d512d1e6701eaf19c4c9e26119f79de0a4
Kostiantyn <kotkotkot@i.ua>
2016-01-24 13:54:25 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,18 @@
-
+<?php
+// Create connection
+$connection = new mysqli(localhost, admin, admin, main);
+// Check connection
+if ($connection->connect_error) {
+    die("Connection failed: " . $connection->connect_error);
+} 
+$sql = "SELECT * FROM main";
+$result = $connection->query($sql);
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+         echo "Hello, " . $row["keyword"]. "<br> current build is build#";
+    }
+} else {
+    echo "0 results";
+}
+$connection->close();
+?>
